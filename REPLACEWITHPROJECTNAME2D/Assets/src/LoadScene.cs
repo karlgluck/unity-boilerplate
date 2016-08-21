d77cd35a8fd9a99378c11aed76896dc0e48c154a
Karl Gluck <karlgluck@gmail.com>
2016-08-21 13:08:32 -0700

    'splash' automatically loads 'main'

---
+++ REPLACEWITHPROJECTNAME2D/Assets/src/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour
+{
+    [UnityEngine.SerializeField]
+    private string levelName;
+
+    void Start ()
+    {
+        Application.LoadLevelAsync(this.levelName);
+	}
+}
