812a1c9c2bd4e0bf541a98b9fd04eef01b538389
Karl Gluck <karlgluck@gmail.com>
2017-04-09 16:02:52 -0700

    Array and vector2 SUL

---
+++ StandardUnity/Ext/ArrayExt.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static partial class ArrayExt
+{
+    public static T Last (this T[] self)
+    {
+        return self.Length > 0 ? self[self.Length - 1] : default(T);
+    }
+
+    public static T SafeIndex<T>(this T[] self, int i)
+    {
+        if (self != null)
+        {
+            int max = self.Length - 1;
+            if (max >= 0)
+            {
+                return array[i < 0 ? 0 : (i > max ? max : i)];
+            }
+        }
+        return default(T);
+    }
+}
---
+++ StandardUnity/Ext/ArrayListExt.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static partial class ArrayListExt
+{
+    public static object Last (this ArrayList self)
+    {
+        return self.Count > 0 ? self[self.Count - 1] : null;
+    }
+
+    public static T Last<T> (this ArrayList self)
+    {
+        return (T)self.Last ();
+    }
+
+    public static T SafeIndex<T> (this ArrayList self, int i)
+    {
+        return (T)self.SafeIndex(i);
+    }
+
+    public static object SafeIndex (this ArrayList self, int i)
+    {
+        if (self != null)
+        {
+            int max = self.Count - 1;
+            if (max >= 0)
+            {
+                return array[i < 0 ? 0 : (i > max ? max : i)];
+            }
+        }
+        return null;
+    }
+    
+}
